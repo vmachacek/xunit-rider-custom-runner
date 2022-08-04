c21838d92a8b2fa2597b1ee080f2243aa4b6ff4b
Vojtech Machacek <vmachacek@gmail.com>
2022-08-04 15:44:18 -0400

    init

---
+++ TestProject2/MyMessageSink.cs
@@ -0,0 +1,25 @@
+﻿using Xunit.Abstractions;
+
+namespace TestProject2;
+
+public class MyMessageSink: IMessageSink
+{
+    public bool OnMessage(IMessageSinkMessage message)
+    {
+        // Do what you want to in response to events here.
+        //
+        // Each event has a corresponding implementation of IMessageSinkMessage.
+        // See examples here: https://github.com/xunit/abstractions.xunit/tree/master/src/xunit.abstractions/Messages
+        if (message is ITestPassed tp)
+        {
+            Console.WriteLine("Execution time was an awesome " + ((ITestPassed)message).ExecutionTime);
+            // Beware that this message won't actually appear in the Visual Studio Test Output console.
+            // It's just here as an example. You can set a breakpoint to see that the line is hit.
+            
+        }
+
+        Console.WriteLine("asd");
+        // Return `false` if you want to interrupt test execution.
+        return true;
+    }
+}
---
+++ TestProject2/MyRunnerReporter.cs
@@ -0,0 +1,23 @@
+﻿using Xunit.Abstractions;
+
+namespace TestProject2;
+
+public class MyRunnerReporter : IRunnerReporter
+{
+    public string Description => "My custom runner reporter";
+
+    // Hard-coding `true` means this reporter will always be enabled.
+    //
+    // You can also implement logic to conditional enable/disable the reporter.
+    // Most reporters based this decision on an environment variable.
+    // Eg: https://github.com/xunit/xunit/blob/cbf28f6d911747fc2bcd64b6f57663aecac91a4c/src/xunit.runner.reporters/TeamCityReporter.cs#L11
+    public bool IsEnvironmentallyEnabled => true;
+
+    public string RunnerSwitch => null;
+
+    public IMessageSink CreateMessageHandler(IRunnerLogger logger)
+    {
+        throw new Exception("CreateMessageHandler");
+        return new MyMessageSink();
+    }
+}
